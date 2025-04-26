bcf8f8efff8f40de86d040466eb27d073f524841
NotToday0007 <rifatkhan.khan002@gmail.com>
2025-04-26 10:49:15 +0600

    added

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,6 @@ use App\Http\Controllers\FeedbackController;
 
 Route::get('/feedback', [FeedbackController::class, 'showForm']);
 Route::post('/feedback', [FeedbackController::class, 'submitFeedback'])->name('feedback.submit');
+Route::get('/', function () {
+    return view('welcome'); // make sure welcome.blade.php exists in resources/views
+});
